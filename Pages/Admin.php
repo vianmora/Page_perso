4b7a6618f8071e9109a0f72ede31ebc268b11139
vianmora <vianmora@yahoo.fr>
2018-04-10 22:53:17 +0200

    version 1.0 : des tutos et des sites utiles pour la vie à l'ECM

---
+++ Pages/Admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Vianney MORAIN page admin</title>
+
+
+		<div class = "stylesheet">
+			<link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+			<link rel="stylesheet" href="Stylesheet/Global.css" />
+			<link rel="stylesheet" href="Stylesheet/index.css" />
+			<!--<link rel="stylesheet" href="Stylesheet/Au survol.css" />
+			<link rel="stylesheet" href="Stylesheet/Au clic.css" />-->
+
+		</div>
+
+		<meta charset = "utf-8">
+	</head>
+
+	<body>
+		<header>
+			<h1>Page admin</h1>
+		</header>
+
+
+		<section>
+			que voulez-vous faire :
+			<ul>
+				<li>
+					ajouter un article au blog 
+				</li>
+				<li>
+					modifier un article du blog (pas encore développé)
+				</li>
+			</ul>
+		</section>
+
+		<footer>
+			<p><a href = "Pages/CV.html" id = "CV"> Acces à mon CV</a></p>
+			<nav>
+				<ul>
+					<li id = "contact" > <a href = "Pages/contact.html" >me contacter|</a></li>
+					<li><a href="https://www.facebook.com/vianney.morain/"><img src="Images_site/Facebook.png" alt="Facebook"  /></a> </li>
+					<li><a href="https://twitter.com/vianney.morain"><img src="Images_site/Twitter.png" alt="Twitter"  /></a></li>
+					<li><a href="https://fr.linkedin.com/in/vianney-morain/"><img src="Images_site/LinkedIn.png" alt="LinkedIn" /></a></li>
+				</ul>
+			</nav>
+		</footer>
+
+	</body>
+
+</html>
---
+++ Pages/templates/footer.php
@@ -0,0 +1,11 @@
+
+
+<p><a href = "Pages/CV.html" id = "CV"> Acces à mon CV</a></p>
+<nav>
+  <ul>
+    <li id = "contact" > <a href = "Pages/contact.html" >me contacter|</a></li>
+    <li><a href="https://www.facebook.com/vianney.morain/"><img src="Images_site/Facebook.png" alt="Facebook"  /></a> </li>
+    <li><a href="https://twitter.com/vianney.morain"><img src="Images_site/Twitter.png" alt="Twitter"  /></a></li>
+    <li><a href="https://fr.linkedin.com/in/vianney-morain/"><img src="Images_site/LinkedIn.png" alt="LinkedIn" /></a></li>
+  </ul>
+</nav>
