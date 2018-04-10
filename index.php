ada8b246d73b79d4a51a97196d2137dfc69bd9a9
vianmora <vianmora@yahoo.fr>
2018-04-10 23:17:01 +0200

    ajout .gitignore

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 			<a href = "" id = "SUF"><img src = "Images_site/Scouts.jpg" alt ="Scoutisme"><figcaption>Scoutisme</figcaption></a>
 
 			<img id = "ECM_Logo" src = "Images_site/ECM_Logo.png" alt ="Logo centrale marseille">
-
+ 
 			<a href = "https://www.phytv.ninja/" id = "Phy-TV_Logo"><img src = "Images_site/Phy-TV_Logo.jpg" alt ="Phy TV"><figcaption>Phy-TV</figcaption></a>
 			<a href = "https://forum-foceen.centrale-marseille.fr/" id = "FOCEEN_Logo"><img src = "Images_site/FOCEEN_Logo.png" alt ="FOCEEN"><figcaption>FOCEEN</figcaption></a>
 			<a href = "https://assos.centrale-marseille.fr/isf/accueil" id = "ISF_Logo"><img src = "Images_site/ISF_Logo.png" alt ="ISF_Logo"><figcaption>ISF</figcaption></a>
